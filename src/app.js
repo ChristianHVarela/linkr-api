2314097021b0b614348d368bbb40d3b0b579b126
encarnacao <cencarnacaoq@gmail.com>
2023-03-07 13:35:13 -0300

    feat: use hashtag router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import hashtagsRouter from "./routers/hashtags.js"
 
 dotenv.config()
 const server = express()
 server.use(cors())
 server.use(express.json())
+server.use(hashtagsRouter)
 
 server.listen(process.env.PORT || 5000, () => {
     console.log("server is fun");
