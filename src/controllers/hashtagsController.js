f916ad7134e153a608e9f53419b34d577d952494
encarnacao <cencarnacaoq@gmail.com>
2023-03-07 13:53:43 -0300

    feat: add function to get posts by hashtag

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -17,4 +17,30 @@ async function getTrending(_, res) {
 	}
 }
 
-export { getTrending };
+async function getHashtag(req, res) {
+	try {
+		const { hashtag } = req.params;
+		const { rows } = await db.query(
+			`SELECT p.id, p.text, p.link, u.name, u.image, COUNT(*) AS num_likes
+            FROM posts p 
+            JOIN posts_hashtags ph ON p.id = ph.post_id
+            JOIN hashtags h ON h.id = ph.hashtag_id
+            JOIN users u ON u.id = p.user_id
+            JOIN posts_likes pl ON p.id = pl.post_id
+            WHERE h.name = $1
+            GROUP BY p.id, p.text, p.link, u.name, u.image
+            ORDER BY p.id DESC;`,
+			[hashtag]
+		);
+		if (rows.length === 0) {
+			res.sendStatus(404);
+			return;
+		}
+		res.send(rows);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+}
+
+export { getTrending, getHashtag };
