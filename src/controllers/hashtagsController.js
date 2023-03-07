d6a6dcafb8b65d49f38daf323dce828fb7708be7
encarnacao <cencarnacaoq@gmail.com>
2023-03-07 13:34:42 -0300

    feat: add function to get trending hashtags

---
+++ src/controllers/hashtagsController.js
@@ -0,0 +1,19 @@
+import { db } from "../config/database.connection.js";
+
+async function getTrending(_, res) {
+	try {
+		const { rows } = await db.query(
+			`SELECT h.id, h.name, COUNT(*) AS num_posts 
+            FROM hashtags h 
+            JOIN posts_hashtags ph ON h.id = ph.hashtag_id
+            GROUP BY h.id, h.name
+            ORDER BY num_posts DESC;`
+		);
+		res.send(rows);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+}
+
+export { getTrending };
