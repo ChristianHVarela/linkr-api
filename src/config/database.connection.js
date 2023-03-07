3ce8234ce74b15d3503240bea2c9b2f5e98cb791
encarnacao <cencarnacaoq@gmail.com>
2023-03-07 13:07:44 -0300

    feat: create database connection config

---
+++ src/config/database.connection.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+const configDatabase = {
+	connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "production") {
+	configDatabase.ssl = true;
+}
+
+const db = new Pool(configDatabase);
+
+export { db };
