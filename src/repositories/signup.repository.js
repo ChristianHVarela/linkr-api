f5b2c1afa1804de027d501433632599c0a7d9d43
Guilherme Tchornei <115326364+GuilhermeTchornei@users.noreply.github.com>
2023-03-07 21:06:55 -0300

    fix: database connection in signup repository

--- src/repositories/signup.repository.js
+++ src/repositories/signup.repository.js
@@ -1,4 +1,4 @@
-import connection from "../config/database.js";
+import { db } from '../config/database.connection.js'
 
 export async function signupRepository(email, password, name, image) {
     await connection.query(`INSERT INTO users (email, password, name, image) VALUES ($1, $2, $3, $4) RETURNING id`, [email, password, name, image]);
