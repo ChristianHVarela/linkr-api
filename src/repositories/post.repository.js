b6e8d8ee31152d7d8194f08fcb6a352e227136ed
expeditus303 <ricardotonietto@gmail.com>
2023-03-09 19:03:02 -0300

    fix: merge

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -1,17 +1,12 @@
 import { db } from '../config/database.connection.js'
 
 export const insertPost = (user_id, link, description) => {
-<<<<<<< HEAD
-    return db.query('INSERT INTO posts (user_id, link, description) VALUES ($1, $2, $3) RETURNING *', [user_id, link, description])
-=======
     return db.query('INSERT INTO posts (user_id, link, description) VALUES ($1, $2, $3)', [user_id, link, description])
 }
 
 export const getPostsOrderByCreatedAtDesc = () => {
     return db.query('SELECT p.*, u.name AS user_name, u.image AS image_profile FROM posts p JOIN users u ON u.id = p.user_id ORDER BY p.created_at DESC LIMIT 20')
-<<<<<<< HEAD
->>>>>>> 7b6f3413bd830e0ff2a2d02cca08fbbe6f3a4ad0
-=======
+
 }
 
 export const findPostById = (id) => {
@@ -34,5 +29,4 @@ export const deletePostById = async (postId) => {
 
 export const updatePostById = (postId, description) => {
     return db.query('UPDATE posts SET description = $1 WHERE id = $2', [description, postId])
->>>>>>> 81ceab70f42acbc019d62c4d8af398d9d443c835
 }
