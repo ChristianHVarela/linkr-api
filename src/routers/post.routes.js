530c0b9654fcf52caaec88c02fca5ccb9fbb3968
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 01:57:57 -0300

    feat: add delete post by id function

--- src/routers/post.routes.js
+++ src/routers/post.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/post.controller.js";
+import { createPost, deletePost, getPosts } from "../controllers/post.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { validateSchema } from "../middlewares/schema.validator.js";
 import { postSchema } from "../schemas/post.shema.js";
 
@@ -8,3 +9,4 @@ export const postRouter = Router()
 
 postRouter.post('/posts', validateSchema(postSchema), createPost)
 postRouter.get('/timeline', getPosts)
+postRouter.delete('/posts/:id', authMiddleware, deletePost)	
