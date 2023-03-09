ef84a80fed40b856722d50a75d8d3b96fc23d85e
ChristianVarela <christian_varela@outlook.com.br>
2023-03-08 23:54:15 -0300

    fix: ajuste na busca de posts da timeline

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -5,5 +5,5 @@ export const insertPost = (user_id, link, description) => {
 }
 
 export const getPostsOrderByCreatedAtDesc = () => {
-    return db.query('SELECT * FROM posts ORDER BY created_at DESC LIMIT 20')
+    return db.query('SELECT p.*, u.name AS user_name, u.image AS image_profile FROM posts p JOIN users u ON u.id = p.user_id ORDER BY p.created_at DESC LIMIT 20')
 }
