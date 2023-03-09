58c06219f9e3aab88f5414d05ed58efb616a92e4
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 00:14:08 -0300

    refactor: fix identation

--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -1,19 +1,20 @@
 import { db } from "../config/database.connection.js";
 
 function trendingRepository() {
-	return db.query(
-		`SELECT h.id, h.name, COUNT(*) AS num_posts 
+	return db.query(`
+        SELECT h.id, h.name, COUNT(*) AS num_posts 
         FROM hashtags h 
         JOIN posts_hashtags ph ON h.id = ph.hashtag_id
         GROUP BY h.id, h.name
         ORDER BY num_posts DESC
-        LIMIT 10;`
-	);
+        LIMIT 10;
+        `);
 }
 
 function getHashtagRepository(hashtag) {
 	return db.query(
-		`SELECT p.id, p.text, p.link, u.name, u.image, COUNT(*) AS num_likes
+		`
+        SELECT p.id, p.text, p.link, u.name, u.image, COUNT(*) AS num_likes
         FROM posts p 
         JOIN posts_hashtags ph ON p.id = ph.post_id
         JOIN hashtags h ON h.id = ph.hashtag_id
@@ -21,7 +22,8 @@ function getHashtagRepository(hashtag) {
         JOIN posts_likes pl ON p.id = pl.post_id
         WHERE h.name = $1
         GROUP BY p.id, p.text, p.link, u.name, u.image
-        ORDER BY p.id DESC;`,
+        ORDER BY p.id DESC;
+        `,
 		[hashtag]
 	);
 }
