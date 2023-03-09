33291b0d739683f3c2bdf432ed074824964b0f21
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 01:20:51 -0300

    fix: change join type and fix column name

--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -13,14 +13,14 @@ function trendingRepository() {
 
 function getHashtagRepository(hashtag) {
 	return db.query(`
-        SELECT p.id, p.text, p.link, u.name, u.image, COUNT(*) AS num_likes
+        SELECT p.id, p.description, p.link, u.name, u.image, COALESCE(COUNT(pl.post_id),0) AS num_likes
         FROM posts p 
         JOIN posts_hashtags ph ON p.id = ph.post_id
         JOIN hashtags h ON h.id = ph.hashtag_id
         JOIN users u ON u.id = p.user_id
-        JOIN posts_likes pl ON p.id = pl.post_id
+        LEFT JOIN posts_likes pl ON p.id = pl.post_id
         WHERE h.name = $1
-        GROUP BY p.id, p.text, p.link, u.name, u.image
+        GROUP BY p.id, p.description, p.link, u.name, u.image
         ORDER BY p.id DESC;
         `,
 		[hashtag]
