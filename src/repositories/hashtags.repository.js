49613141d328cb6585cd5d141294b57906ace75e
encarnacao <cencarnacaoq@gmail.com>
2023-03-09 10:21:15 -0300

    feat: add functions to handle hashtags addition and deletion

--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -12,7 +12,8 @@ function trendingRepository() {
 }
 
 function getHashtagRepository(hashtag) {
-	return db.query(`
+	return db.query(
+		`
         SELECT p.id, p.description, p.link, u.name, u.image, COALESCE(COUNT(pl.post_id),0) AS num_likes
         FROM posts p 
         JOIN posts_hashtags ph ON p.id = ph.post_id
@@ -22,9 +23,34 @@ function getHashtagRepository(hashtag) {
         WHERE h.name = $1
         GROUP BY p.id, p.description, p.link, u.name, u.image
         ORDER BY p.id DESC;
-        `,
-		[hashtag]
-	);
+        `,[hashtag]);
 }
 
-export { trendingRepository, getHashtagRepository };
+async function addHashtag(hashtagName, postId) {
+	try {
+		await db.query('BEGIN');
+		const { rows: hashtag } = await db.query(`
+        INSERT INTO hashtags (name)
+        VALUES ($1)
+        ON CONFLICT (name) DO UPDATE SET name = excluded.name
+        RETURNING *;
+        `,[hashtagName]);
+		await db.query(`
+		INSERT INTO posts_hashtags (post_id, hashtag_id)
+		VALUES ($1, $2);
+		`,[postId, hashtag[0].id]);
+		await db.query('COMMIT');
+	} catch (e) {
+		await db.query('ROLLBACK');
+		throw new Error(`Erro ao adicionar hashtag: ${e.message}`);
+	}
+}
+
+function deleteHashtags(postId) {
+	return db.query(`
+        DELETE FROM posts_hashtags
+        WHERE post_id = $1;
+        `,[postId]);
+}
+
+export { trendingRepository, getHashtagRepository, addHashtag, deleteHashtags };
