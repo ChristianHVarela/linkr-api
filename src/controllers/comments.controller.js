3b5fde647d012b2c02a1dac26655ff5ea45c8d7e
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-03-25 13:24:23 -0300

    feat: get comments route and delete comments when delete post

--- src/controllers/comments.controller.js
+++ src/controllers/comments.controller.js
@@ -1,4 +1,4 @@
-import { postCommentQuery } from "../repositories/comments.repository.js";
+import { getCommentsByPost, postCommentQuery } from "../repositories/comments.repository.js";
 import { findPostById } from "../repositories/post.repository.js";
 
 export async function PostComment(req, res) {
@@ -12,9 +12,23 @@ export async function PostComment(req, res) {
     {
         const { rows:post } = await findPostById(postId);
         if (post.length === 0) return res.status(404).send("post not found");
-
         await postCommentQuery(userId, postId, comment);
         res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
+export async function GetComments(req, res) {
+    const { id: userId } = res.locals.user;
+    const { id: postId } = req.params;
+
+    if (isNaN(postId)) return res.sendStatus(401);
+
+    try {
+        const { rows: comments } = await getCommentsByPost(userId, postId);
+        res.status(200).send(comments);
     } catch (error) {
         console.log(error);
         res.status(500).send(error);
--- src/repositories/comments.repository.js
+++ src/repositories/comments.repository.js
@@ -3,3 +3,14 @@ import { db } from "../config/database.connection.js";
 export async function postCommentQuery(userId, postId, comment) {
     return await db.query('INSERT INTO comments (user_id, post_id, comment) VALUES ($1, $2, $3);', [userId, postId, comment]);
 }
+
+export async function getCommentsByPost(userId, postId) {
+    return await db.query(`
+    SELECT c.user_id, u.name AS name, u.image AS image, c.comment, p.user_id = c.user_id AS post_author, f.id IS NOT NULL AS following
+        FROM comments c
+        JOIN users u ON c.user_id = u.id
+        JOIN posts p ON c.post_id = p.id
+        LEFT JOIN followers f ON f.user_id = $1 and f.following_id = c.user_id
+    WHERE c.post_id = $2
+    ORDER BY c.id ASC;`, [userId, postId]);
+}
